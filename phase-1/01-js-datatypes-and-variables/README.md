d4213bbd26437126f3c6fc62edebaca70e931064
Louis Michael Medina <louis@Louis-Medinas-MacBook-Pro.local>
2021-09-20 13:33:33 -0400

    first commit

---
+++ phase-1/01-js-datatypes-and-variables/README.md
@@ -0,0 +1,28 @@
+# JavaScript Data Types and Variables
+
+### Goals
+- Utilizing `console.log()` for testing
+- Identify JS Data Types
+- Define variables using let vs const 
+- Understand variable scoping
+- Practice conditional statements 
+- Don't let JS fool you! {JS Gotcha's}
+
+#### Identify JS Data Types 
+- What are the 8 data types used in JS?
+- How to use `typeof`
+- Primitive vs Non-Primitive data types
+- Operating on data using operators 
+
+#### JavaScript Variables
+- Difference between `let` vs `const`
+- How to properly name variables
+- Variable scopes 
+- Identifying and reading variable errors
+
+#### Conditionals
+- Writing conditional statements with:
+    - If, else and else if 
+    - Ternary operators 
+    - Switch statements
+- Use logical operators
---
+++ phase-1/01-js-datatypes-and-variables/index.js
@@ -0,0 +1,93 @@
+
+
+// -------------------------------------------
+
+console.log("Check Your Answers Here!");
+
+// 🚧 Break Out Activity 1: Using let vs. const
+
+// Properly declare "x" using "const"
+
+    const x;
+
+    const x = 5;
+
+    const x;
+
+    console.log(`Success! x = ${x}`);
+
+// Properly declare "y" using "let"
+
+    // 🚨 Uncomment the code below to continue:
+
+    // let y;
+
+    // let y = 5; 
+
+    // let y;
+
+    // console.log(`Success! y = ${y}`);
+
+// Refactor the code below to get a successful response in console
+
+    // 🚨 Uncomment the code below to continue:
+
+    // const a = 2;
+    
+    // a = 2;
+    
+    // let b;
+    
+    // let b = 4
+    
+    // console.log(`Success! ${a} + ${a} = ${b}`);
+
+// 🚧 Break Out Activity 2: Using if...else
+
+    // 🚨 Uncomment the code below to continue:
+
+    // 🚧 Set a value for "time" between 0 and 24
+    // let time;
+
+    // 💡 If "time" is less than or equal to 12...
+    // if (❓) {
+
+        // console.log(`${time} is in the AM.`)
+
+    // 💡 In all other cases...
+    // } else {
+
+        // console.log(`${time} is in the PM.`)
+
+    // }
+
+    // ✨ BONUS: Try refactoring the above expression using a ternary operator.
+
+// 🚧 Break Out Activity 3: Using if...else...if
+
+    // We are given a variable "marks." Our task is to print:
+
+    // - "AA" if marks is greater than 90.
+    // - "AB" if marks is greater than 80 and less than or equal to 90.
+    // - "BB" if marks is greater than 70 and less than or equal to 80.
+    // - "BC" if marks is greater than 60 and less than or equal to 70.
+    // - "CC" if marks is greater than 50 and less than or equal to 60.
+    // - "CD" if marks is greater than 40 and less than or equal to 50.
+    // - "DD" if marks is greater than 30 and less than or equal to 40.
+    // - "FF" if marks is less than or equal to 30.
+
+    // 🚨 Uncomment the code below to continue
+
+    // 🚨 Assign a value to "marks" between 0 and 100
+    // let marks;
+
+    // 🚨 Format your if...else if...else conditional below
+    // if () {
+
+    // } else if () {
+
+    // ...
+    
+    // } else {
+
+    // }
