9b8099529ab7b8154cb239feed7b1f534670b233
Louis Michael Medina <louis@Louis-Medinas-MacBook-Pro.local>
2021-09-24 13:36:05 -0400

    add starter code for dom-manipulation

---
+++ phase-1/05-dom-manipulation/README.md
@@ -0,0 +1,27 @@
+# DOM Manipulation
+
+### Learning Goals
+
+&emsp; ✅ Explore how to traverse the DOM tree
+
+&emsp; ✅ Create, read, update, and delete elements from the DOM by using: 
+- `.querySelector()`
+- `.querySelectorAll()`
+- `.createElement()`
+- `.getElementById()`
+- `.getElementsByClassName()`
+- `.append()`
+- `.remove()`
+- `.innerHTML` vs. `.innerText` vs. `.textContent`
+
+### Warm Up Question
+
+&emsp; 💡 What are the limitations of arrays that objects help us to address?
+
+### Resources
+
+&emsp; [CodeAcademy - Learn Javascript: Objects Cheatsheet](https://www.codecademy.com/learn/game-dev-learn-javascript-objects/modules/game-dev-learn-javascript-objects/cheatsheet)
+
+&emsp; [The Modern JavaScript Tutorial - Prototypal Inheritance](https://javascript.info/prototype-inheritance)
+
+&emsp; [The Essential Guide to JavaScript Iterators](https://www.javascripttutorial.net/es6/javascript-iterator/)
---
+++ phase-1/05-dom-manipulation/index.js
@@ -0,0 +1,156 @@
+// ✅ What is the DOM?
+
+	// Chrome Dev Tools
+
+// ✅ Creating / Reading DOM Elements
+
+    // .createElement() => creates one DOM element
+
+	// .querySelector() => returns the first matching DOM element
+
+	// .querySelectorAll() => returns an array of matching DOM elements
+
+	// .getElementById() => returns the DOM element with matching Id
+
+	// .getElementsByClassName() => returns an array of DOM elements with matching Class
+
+	// .getElementsByTagName() => when is this especially useful?
+        
+// ✅ Updating / Deleting DOM Elements
+        
+	// .append
+
+	// .remove()
+
+// ✅ .innerHTML vs. .innerText vs. .textContent
+
+  	// .innerHTML => if you want to insert HTML within a DOM element
+
+	// .innerText => if you only want to insert text, shows only human-readable elements
+
+	// .textContent => if you only want to insert text, shows formatting of elements
+
+// -------------------------------------------
+
+console.log("------------------------");
+console.log("⬇️ Break Out Activites ⬇️");
+console.log("🚨 Comment Out Lecture Code Above Before Starting 🚨");
+console.log("💡 Use console.log() To Check Answers 💡");
+console.log("------------------------");
+
+// 🚧 Break Out Activity 1: Creating / Reading / Updating DOM Elements
+
+  // 🚨 Comment out any conflicting code above before proceeding.
+
+		// array of pokemon objects
+		const pokemon = [
+			{
+				id: 1,
+				name: "Bulbasaur",
+				img: "./images/bulbasaur.png",
+				likes: 4,
+			},
+			{
+				id: 2,
+				name: "Ivysaur",
+				img: "./images/ivysaur.png",
+				likes: 21,
+			},
+			{
+				id: 3,
+				name: "Venusaur",
+				img: "./images/venasaur.png",
+				likes: 7,
+			},
+			{
+				id: 4,
+				name: "Charmander",
+				img: "./images/charmander.png",
+				likes: 20,
+			},
+			{
+				id: 5,
+				name: "Charmeleon",
+				img: "./images/charmeleon.png",
+				likes: 11,
+			},
+		];
+
+		// container to store DOM element with ID of "poke-container" => <div id="poke-container"></div>
+		const pokeContainer = document.getElementById("poke-container");
+
+  // 1️⃣ Given "pokemon" and "pokeContainer" above, create a function (renderPokemon) that:
+
+	//  ✔️ Accepts a single "pokemon" object
+ 
+ 	//  ✔️ Creates a "div" element to contain information about each Pokemon and stores the "div" in a variable (pokeCard)
+		//	Assign pokeCard a unique ID with the Pokemon's "id" prepended by "poke-" (i.e., "poke-1" for Bulbasaur)
+		//	Assign pokeCard a className of "poke-card"
+	
+	//  ✔️ Creates an "img" element for each Pokemon and stores the "img" in a variable (pokeImg)
+		//	Assign pokeImg a source (src) linking to the Pokemon's image
+		// 	Assign pokeImg a unique alt with the Pokemon's name followed by " image" (i.e., "bulbasaur image")
+	
+	//	✔️ Creates an "h3" element for each Pokemon and stores the element in a variable (pokeName)
+		// 	Use textContent() to fill in the heading with the Pokemon's name
+
+	//	✔️ Appends all variables to "pokeCard" before appending "pokeCard" to "pokeContainer"
+		// 	Use append() as necessary
+
+	// 	💡 Use Chrome Dev Tools to view changes being made to the DOM
+
+	// 	💡 Take a look at "styles.css" to see how the DOM Elements we're building out correspond to CSS class and ID selectors.
+
+	// 	✨ BONUS: Try to reduce redudancy (DRY Principle) in your code through the use of JS functions and variables.
+
+
+
+
+
+	// ✅ Check Answer: 
+	// function init() {
+	// 	pokemon.forEach(renderPokemon);
+	// }
+
+	// init();
+
+// 🚧 Break Out Activity 2: Creating / Reading / Updating Elements
+
+  // 🚨 Comment out any conflicting code above before proceeding.
+
+  // 1️⃣ Add the following capabilities to renderPokemon:
+
+	//  ✔️ Creates an "h3" element for each Pokemon and stores the element in a variable (pokeLikes)
+		// 	Use textContent() to fill in the heading with "Likes: "
+	
+	//  ✔️ Creates an "h5" element for each Pokemon and stores the element in a variable (likesNum)
+		// 	Assign likesNum a className of "likes-num"
+		// 	Use textContent() to fill in the heading with the Pokemon's number of likes
+
+	//  ✔️ Creates a "button" element for each Pokemon and stores the element in a variable (likesBttn)
+		// 	Assign likesBttn a className of "likes-bttn"
+		// 	Use textContent() to fill in the button with a label, "♥"
+
+	//  ✔️ Creates a "button" element for each Pokemon and stores the element in a variable (deleteBttn)
+		// 	Assign deleteBttn a className of "delete-bttn"
+		// 	Use textContent() to fill in the button with a label, "Delete"
+
+	//	✔️ Appends all variables to "pokeCard" before appending "pokeCard" to "pokeContainer"
+		// 	Use append() as necessary
+
+	// 	💡 Use Chrome Dev Tools to view changes being made to the DOM
+	
+	// 	💡 Take a look at "styles.css" to see how the DOM Elements we're building out correspond to CSS class and ID selectors.
+
+	// 	✨ BONUS: Try to reduce redudancy (DRY Principle) in your code through the use of JS functions and variables.
+
+
+
+
+
+	// ✅ Check Answer: 
+	// function init() {
+	// 	pokemon.forEach(renderPokemon);
+	// }
+
+	// init();
