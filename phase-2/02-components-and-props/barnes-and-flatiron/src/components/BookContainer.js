4d7c2602788396694174149240106ff07e837a46
ixnp <enprocopios@gmail.com>
2021-10-12 16:24:58 -0700

    fix spelling

---
+++ phase-2/02-components-and-props/barnes-and-flatiron/src/components/BookContainer.js
@@ -0,0 +1,14 @@
+import BookCard from "./BookCard"
+
+function BookContainer(){
+    //TODO: Render BookCard dynamically for every book in props
+    return(
+        <>
+            <BookCard />
+            <BookCard />
+            <BookCard />
+        </>
+    )
+}
+
+export default BookContainer
