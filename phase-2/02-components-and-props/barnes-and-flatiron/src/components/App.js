f8ec2f6ab6a660a4450aac50197c0ab89cda7e14
ixnp <enprocopios@gmail.com>
2021-10-13 09:24:47 -0700

    adding image to readme

---
+++ phase-2/02-components-and-props/barnes-and-flatiron/src/components/App.js
@@ -0,0 +1,13 @@
+import BookContainer from "./BookContainer";
+
+function App() {
+  //TODO: Add Header, Genre, Form
+  //TODO: Pass BookContainer our book data through props
+  return (
+    <div className="App">
+      <BookContainer />
+    </div>
+  );
+}
+
+export default App;
