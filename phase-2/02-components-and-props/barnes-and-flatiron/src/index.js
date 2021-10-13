5597a95d50b974f52fc44e73be7f334ad34b0476
ixnp <enprocopios@gmail.com>
2021-10-13 07:36:36 -0700

    fixing bug in index

--- phase-2/02-components-and-props/barnes-and-flatiron/src/index.js
+++ phase-2/02-components-and-props/barnes-and-flatiron/src/index.js
@@ -1,18 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-// const element = <div>hi</div>;
-// console.log(element)
-// ReactDOM.render(element, document.getElementById('root'));
+import App from './components/App';
 
-// function App(props){
-//   console.log(props)
-//   return(
-//     <div>Hi from {props.name}</div>
-//   )
-// };
-
-// ReactDOM.render(<App name='rose'/>, document.getElementById('root'));
-//App({name:'rose'})
 
 ReactDOM.render(<App />,document.getElementById('root'));
