f6a21ccfc5e8155b46816e559fc33dba806fee88
ixnp <enprocopios@gmail.com>
2021-10-18 10:06:28 -0700

    removed extra deliverable

--- phase-2/05-forms/barnes-and-flatiron/src/components/Form.js
+++ phase-2/05-forms/barnes-and-flatiron/src/components/Form.js
@@ -2,42 +2,48 @@ function Form(){
     //TODO: control the form inputs with local state
     //TODO: on submit lift the form data to APP
     return(
-        <form style={{display:"flex", flexDirection:"column", width:"400px", margin:"auto"}}>
+         <form style={{display:"flex", flexDirection:"column", width:"400px", margin:"auto"}}>
             <label>Title</label>
             <input 
             type="text"
+            name="title"
             aria-label="title"
-            aria-required="true"
+           
             ></input>
             <label>Author</label>
             <input
             type="text"
+            name="author"
             aria-label="title"
-            aria-required="true"
+           
             ></input>
             <label>Genre</label>
             <input
             type="text"
+            name="genre"
             aria-label="title"
-            aria-required="true"
+           
             ></input>
             <label>Image</label>
             <input
             type="text"
+            name="image"
             aria-label="title"
-            aria-required="true"
+           
             ></input>
             <label>Price</label>
             <input
             type="text"
+            name="price"
             aria-label="title"
-            aria-required="true"
+           
             ></input>
             <label>Description</label>
             <textarea
             type="text"
+            name="description"
             aria-label="title"
-            aria-required="true"
+           
             ></textarea>
             <input
             type="submit"
