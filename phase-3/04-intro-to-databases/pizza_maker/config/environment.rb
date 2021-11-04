0adcb58c6789d3c6c39fe0dc84f3db47519c8330
Aysan Isayo <aisayo@icloud.com>
2021-11-04 09:37:11 -0700

    breakout exercise

--- phase-3/04-intro-to-databases/pizza_maker/config/environment.rb
+++ phase-3/04-intro-to-databases/pizza_maker/config/environment.rb
@@ -1,6 +1,12 @@
 require "bundler/setup"
-Bundler.require(:default)
+# DB = SQLite3::Database.new("db/development.db")
+# DB.results_as_hash = true
+
+ENV["RACK_ENV"] ||= "development"
+
+Bundler.require(:default, ENV["RACK_ENV"])
+
 require "date"
 require "active_support/core_ext/integer/time"
 require "active_support/core_ext/date/calculations"
-require_all "lib"
+require_all "app"
