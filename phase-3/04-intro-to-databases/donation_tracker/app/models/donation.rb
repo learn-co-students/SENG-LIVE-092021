9e9b29196984211b769fb958323fac51ea27ed44
Aysan Isayo <aisayo@icloud.com>
2021-11-04 08:13:13 -0700

    added an orm class for example

--- phase-3/04-intro-to-databases/donation_tracker/app/models/donation.rb
+++ phase-3/04-intro-to-databases/donation_tracker/app/models/donation.rb
@@ -1,58 +1,57 @@
-class Donation < ActiveRecord::Base
+class Donation
 
-    # @@all = [] 
+    @@all = [] 
 
-    # attr_accessor :amount, :organization, :date, :completed
-    # attr_reader :id
+    attr_accessor :amount, :organization, :date, :completed
+    attr_reader :id
 
-    # def self.all 
-    #     @@all 
-    # end
-
-    # def self.create(attributes)
-    #     self.new(attributes).save
-    # end
-
-    # def self.primary_key
-    #     if @@all.empty? 
-    #         1
-    #     else 
-    #         @@all[-1].id + 1
-    #     end
-    # end
-
-    # def self.find_by_id(id)
-    #     @@all.find do |donation|
-    #         donation.id == id
-    #     end
-    # end
-
-    def self.pending
-        # @@all.filter do |donation|
-        #     donation.completed == false
-        # end
-        self.where(completed: false)
+    def self.all 
+        @@all 
     end
 
-    # def initialize(organization:, amount:, date:, completed:)
-    #     @id = Donation.primary_key
-    #     @organization = organization 
-    #     @amount = amount 
-    #     @date = date 
-    #     @completed = completed 
-    # end
+    def self.create(attributes)
+        self.new(attributes).save
+    end
 
-    # def read_donation
-    #     puts "Organization: #{@organization}"
-    #     puts "Amount: #{@amount}"
-    #     puts "Date: #{@date}"
-    #     puts "Completed? #{@completed}"
-    # end
+    def self.primary_key
+        if @@all.empty? 
+            1
+        else 
+            @@all[-1].id + 1
+        end
+    end
 
-    # def save 
-    #     @@all << self
-    #     self
-    # end
+    def self.find_by_id(id)
+        @@all.find do |donation|
+            donation.id == id
+        end
+    end
+
+    def self.pending
+        @@all.filter do |donation|
+            donation.completed == false
+        end
+    end
+
+    def initialize(organization:, amount:, date:, completed:)
+        @id = Donation.primary_key
+        @organization = organization 
+        @amount = amount 
+        @date = date 
+        @completed = completed 
+    end
+
+    def read_donation
+        puts "Organization: #{@organization}"
+        puts "Amount: #{@amount}"
+        puts "Date: #{@date}"
+        puts "Completed? #{@completed}"
+    end
+
+    def save 
+        @@all << self
+        self
+    end
 
 
 
